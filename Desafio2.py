c99112d2d005a771170bfd999abe9f40e02e9ff5
Iago-Lunelli <lunelliiago@gmail.com>
2026-04-06 08:01:39 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,5 +1,6 @@
+#Pede para inserir o número que se quer saber
 numero = int(input("Digite um número: "))
-
+#Analise se o número é par, caso contrário ele insere que é Impar
 if numero % 2 == 0:
     print("O número é par")
 else:
