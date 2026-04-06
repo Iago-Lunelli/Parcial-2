29147158cc5d9376e8b8801d11aa1f886bc6bc76
Iago-Lunelli <lunelliiago@gmail.com>
2026-04-06 07:50:15 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,1 +1,7 @@
-
+#Inserir número variável 
+N1 = float(input("Digite o primeiro Número: "))
+N2 = float(input("Digite o segundo Número: "))
+#Soma dos números inseridos
+soma = N1 + N2
+#Mostra o resultado
+print("A soma é: ", soma)
